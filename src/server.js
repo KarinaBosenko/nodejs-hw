2075dc2d569e3af72286a89f2a60aa5c379a0997
Karina Bosenko <karina.bosenko.10.10@gmail.com>
2026-06-05 16:54:42 +0300

    Fix server.js

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT ?? 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
-app.use(cookieParser);
+app.use(cookieParser());
 
 app.use(notesRoutes);
 app.use(authRoutes);
