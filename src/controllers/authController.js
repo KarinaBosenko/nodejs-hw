dabf56035741a3e142b7b070da066c7f523ac8ec
Karina Bosenko <karina.bosenko.10.10@gmail.com>
2026-06-06 10:52:36 +0300

    Fix sendMail

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,7 +7,7 @@ import fs from 'node:fs/promises';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
 import { User } from '../models/user.js';
-import { sendMail } from '../utils/sendMail.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -124,7 +124,7 @@ export const requestResetEmail = async (req, res) => {
   });
 
   try {
-    await sendMail({
+    await sendEmail({
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
